789b79358cfca47eba747463a80d0ce807756079
Jin Chen <jinchen93@gmail.com>
2017-05-16 09:45:22 -0700

    Add sessions controller and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: sessions[:session_token])
+  end
+
+  def login!(user)
+    @current_user = user
+    sessions[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    @current_user = nil
+    sessions[:session_token] = nil
+  end
 end
