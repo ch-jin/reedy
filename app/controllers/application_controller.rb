c089254255d79c56079f2c73f1610c740aa945a7
Jin Chen <jinchen93@gmail.com>
2017-05-16 09:59:37 -0700

    Boostrap current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
@@ -16,5 +16,9 @@ class ApplicationController < ActionController::Base
     current_user.reset_session_token!
     @current_user = nil
     session[:session_token] = nil
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
