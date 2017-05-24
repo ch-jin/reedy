61a5e7ff55b608da5101a555d15b87eb8c8a034e
Jin Chen <jinchen93@gmail.com>
2017-05-23 23:07:20 -0700

    Add path autoloading to the correct module

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.autoload_paths << Rails.root.join('lib')
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Reedy
   class Application < Rails::Application
+    config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
