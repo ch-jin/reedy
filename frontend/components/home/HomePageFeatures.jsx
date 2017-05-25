61e4ae9d97c254ceb9926e5733b0f65ef009bfaa
Jin Chen <jinchen93@gmail.com>
2017-05-25 14:42:48 -0700

    Add home page features information

--- frontend/components/home/HomePageFeatures.jsx
+++ frontend/components/home/HomePageFeatures.jsx
@@ -1,10 +1,55 @@
 import React from "react";
-import { Features } from "../../styles/home";
+import {
+  Features,
+  StyledFeature,
+  FeatureIcon,
+  FeatureText,
+  Tagline,
+  GreyWrapper,
+  FeaturesTitle,
+} from "../../styles/home";
 
 const HomePageFeatures = () => (
-  <Features>
-    Features
-  </Features>
+  <GreyWrapper>
+    <Features>
+
+      <StyledFeature>
+        <FeaturesTitle>
+          Features
+        </FeaturesTitle>
+        <FeatureIcon className="fa fa-feed" />
+        <Tagline>Discover</Tagline>
+        <FeatureText>
+          Discover new RSS feeds from our wide library of sources
+        </FeatureText>
+      </StyledFeature>
+
+      <StyledFeature>
+        <FeatureIcon className="fa fa-calendar-check-o" />
+        <Tagline>Subscriptions</Tagline>
+        <FeatureText>
+          Keep up to date with the most recent articles
+        </FeatureText>
+      </StyledFeature>
+
+      <StyledFeature>
+        <FeatureIcon className="fa fa-history" />
+        <Tagline>Save</Tagline>
+        <FeatureText>
+          Easily save articles to be read at a later time
+        </FeatureText>
+      </StyledFeature>
+
+      <StyledFeature>
+        <FeatureIcon className="fa fa-folder-open-o" />
+        <Tagline>Collections</Tagline>
+        <FeatureText>
+          Create collections to follow and organize RSS feeds
+        </FeatureText>
+      </StyledFeature>
+
+    </Features>
+  </GreyWrapper>
 );
 
 export default HomePageFeatures;
