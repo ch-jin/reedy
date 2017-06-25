a7ad56fa45e0283aa089b7894fa8d8bf5c7a8afa
Jin Chen <jinchen93@gmail.com>
2017-06-24 20:21:31 -0700

    Add reedy test skeleton

---
+++ frontend/__tests__/reedy.js
@@ -0,0 +1,22 @@
+// globals jest
+
+// jest.mock("../store/store", () => jest.fn(() => ({ storeKey: "storeValue" })));
+// import configureStore from "../store/store";
+// import ReactDOM from "react-dom";
+
+// describe("entry", () => {
+//   let Entry, Root, renderedRoot;
+
+//   beforeAll(() => {
+//     document.addEventListener = jest.fn();
+//     document.getElementById = jest.fn(id => id);
+//     ReactDOM.render = jest.fn();
+//     Entry = require("../components/app")
+//     document.addEventListener.mock.calls[0][1]();
+//   });
+
+//   it("listens for the DOMContentLoaded event", () => {
+//     expect(document.addEventListener).toBeCalled();
+//     expect(eventListenerCalls[0][0]).to
+//   });
+// });
